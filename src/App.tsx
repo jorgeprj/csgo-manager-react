2656cef875a74b9d030e4156c4de4ec2ce6f4e27
jorgeprj <jlmp2002@gmail.com>
2023-09-17 01:00:49 -0300

    init routes and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { BrowserRouter } from "react-router-dom"
+import { AppRoutes } from "./routes"
+
 const App = () => {
 	return (
-		<div>App</div>
+		<BrowserRouter>
+			<AppRoutes/>
+		</BrowserRouter>
 	)
 }
 
